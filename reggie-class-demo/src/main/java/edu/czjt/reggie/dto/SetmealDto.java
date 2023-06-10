6c54dad0b3667e8f74367e58880040ca65272db4
18789829806 <hbxylsh@163.com>
2023-06-10 11:38:49 +0800

    reggie(缺失）

---
+++ reggie-class-demo/src/main/java/edu/czjt/reggie/dto/SetmealDto.java
@@ -0,0 +1,14 @@
+package edu.czjt.reggie.dto;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SetmealDto extends Setmeal {
+
+    private List<SetmealDish> setmealDishes;
+
+    private String categoryName;
+}
